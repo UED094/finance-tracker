1c3dd1e7033f6e9b8408d91815f4d08f6c10fb89
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-10-23 19:24:59 -0700

    Header added.

---
+++ expense-tracker-react-bootstrap/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopNavbar from "./TopNavbar";
+
+
+const Header = () => {
+
+    return (
+        <>
+            <TopNavbar />
+        </>
+    );
+};
+
+export default Header;
