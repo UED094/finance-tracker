22afd6a31eb563c69fb5049cd2ae9962800cbc82
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-10-23 19:23:48 -0700

    Added routes to app.js

--- expense-tracker-react-bootstrap/src/App.js
+++ expense-tracker-react-bootstrap/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
-import {Button} from 'react-bootstrap'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./pages/Layout";
+import Support from "./pages/Support";
+import Summary from "./pages/Summary";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <h1>Home</h1>
-        <Button variant={"success"}> Just a button</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+              <Route index element={<Home />}></Route>
+              <Route path="about" element={<About />}></Route>
+              <Route path="support" element={<Support />}></Route>
+              <Route path="summary" element={<Summary />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
