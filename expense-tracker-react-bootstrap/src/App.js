c8f9c9f3957a3c02ede12aeb83e478e3aeafa2d4
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-10-23 18:10:41 -0700

    Included react bootstrap.

--- expense-tracker-react-bootstrap/src/App.js
+++ expense-tracker-react-bootstrap/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import {Button} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <h1>Home</h1>
+        <Button variant={"success"}> Just a button</Button>
     </div>
   );
 }
